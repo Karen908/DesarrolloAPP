8ea1597a7a9e1bf39fe036fd89bec529247ae5ec
Ivan Santiago Sepulveda <ivan.sepulveda3@soy.sena.edu.co>
2024-03-08 03:03:33 -0500

    falta redireccion de register

--- App.tsx
+++ App.tsx
@@ -1,149 +1,40 @@
-import React from 'react';
-import { ScrollView, ImageBackground, StyleSheet, Text, View, TouchableOpacity, TextInput, Button, ToastAndroid, Image } from 'react-native';
-import { RoundedButton } from './src/components/RoundedButton';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { HomeScreen } from './src/views/home/home';
+import { RegisterScreen } from './src/views/register/Register';
 
-const image = require('./assets/imagen.jpeg');
+export type RootStackParamList = {
+  HomeScreen: undefined;
+  RegisterScreen: undefined;
+  };
+  
+  const Stack = createNativeStackNavigator <RootStackParamList>
+  ();
 
-const App = () => (
- <ScrollView style={styles.container}>
-    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-    <View style={styles.headingContainer}>
-  <Text style={[styles.heading, styles.italicText]}>
-    <View style={styles.logoContainer}>
-      <Image source={require('./assets/logo.jpeg')} style={styles.logoImage} />
-      <Text style={styles.logoText}>Karen y Je</Text>
-    </View>
-  </Text>
-</View>
+  const App = () => {
+  return (
 
-      
-    </ImageBackground>
-    <View style={styles.roundedContainer}>
-  <View style={styles.form}>
-    <Text style={styles.formText}>INGRESAR</Text>
-    <View style={styles.formInput}>
-      <Image style={styles.formIcon} source={require('./assets/email.png')} />
-      <TextInput
-        style={styles.formTextInput}
-        placeholder='Correo electrónico'
-        keyboardType='email-address'
-      />
-    </View>
-    <View style={styles.formInput}>
-      <Image style={styles.formIcon} source={require('./assets/password.png')} />
-      <TextInput
-        style={styles.formTextInput}
-        placeholder='Contraseña'
-        keyboardType='default'
-        secureTextEntry={true}
-      />
-    </View>
-    <View style={{ marginTop: 30 }}>
-    <RoundedButton text='ENTRAR' onPress={ () => ToastAndroid.show('HOLA!', 
-    ToastAndroid.SHORT)}/>
-    </View>
-    <View style={styles.formRegister}>
-      <Text>¿No tienes cuenta?</Text>
-      <Text style={styles.formRegisterText}>Regístrate</Text>
-    </View>
-  </View>
-</View>
+  <NavigationContainer>
+  <Stack.Navigator screenOptions={{
+  headerShown: false
+  }}>
+  <Stack.Screen
+  name="HomeScreen"
+  component={HomeScreen}
+  />
+  <Stack.Screen
+  name="RegisterScreen"
+  component={RegisterScreen}
+  options={{
+  headerShown: true,
+  title: 'Registro',
+  }}
+  />
+  </Stack.Navigator>
+  </NavigationContainer>
+  );
+  };
 
- </ScrollView>
-);
 
-const styles = StyleSheet.create({
- container: {
-    flex: 1,
- },
- image: {
-    flex: 1,
-    justifyContent: 'center',
- },
- heading: {
-    color: 'white',
-    fontSize: 22,
-    marginBottom: 400,
-    lineHeight: 54,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    backgroundColor: '#000000c0',
- },
- button: {
-    backgroundColor: 'black',
-    padding: 10,
-    margin: 10,
-    borderRadius: 5,
- },
- buttonText: {
-    color: 'white',
-    fontSize: 18,
-    textAlign: 'center',
- },
- italicText: {
-    fontStyle: 'italic',
- },
- roundedContainer: {
-   borderRadius: 10, 
-   overflow: 'hidden',
-   margin: 10,
- },
- form: {
-    padding: 20,
-    backgroundColor: '#fff',
- },
- formText: {
-    fontSize: 24,
-    marginBottom: 20,
- },
- formInput: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 20,
- },
- formIcon: {
-    width: 20,
-    height: 20,
-    marginRight: 10,
- },
- formTextInput: {
-    flex: 1,
-    borderWidth: 1,
-    borderColor: '#000',
-    borderRadius: 5,
-    padding: 10,
- },
- formRegister: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginTop: 20,
- },
- formRegisterText: {
-    color: 'blue',
-    marginLeft: 5,
- },
- headingContainer: {
-   alignItems: 'center',
-   marginBottom: 2,
-   padding: 25,
- },
- logoContainer: {
-   flexDirection: 'row',
-   marginLeft: 10, 
- },
- logoImage: {
-   width: 30,
-   height: 30,
-   marginLeft:10,
-   marginRight: 10,  
- },
- logoText: {
-   color: 'white',
-   fontSize: 20,
-   fontWeight: 'bold',
- },
- 
- });
-
-export default App;
-
+  export default App;
