d64df3e7462348d1c4587d116561c526e41b49d9
Karen908 <131012640+Karen908@users.noreply.github.com>
2024-03-07 19:12:57 -0500

    Realizo commit con cambios en la configuración de Android Studio para integrar correctamente React Native. Se han descargado paquetes y extensiones necesarios, y se ha optimizado la configuración del emulador para un rendimiento eficiente. A demas, realizo el diseño de un formulario.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,149 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { ScrollView, ImageBackground, StyleSheet, Text, View, TouchableOpacity, TextInput, Button, ToastAndroid, Image } from 'react-native';
+import { RoundedButton } from './src/components/RoundedButton';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+const image = require('./assets/imagen.jpeg');
+
+const App = () => (
+ <ScrollView style={styles.container}>
+    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+    <View style={styles.headingContainer}>
+  <Text style={[styles.heading, styles.italicText]}>
+    <View style={styles.logoContainer}>
+      <Image source={require('./assets/logo.jpeg')} style={styles.logoImage} />
+      <Text style={styles.logoText}>Karen y Je</Text>
     </View>
-  );
-}
+  </Text>
+</View>
+
+      
+    </ImageBackground>
+    <View style={styles.roundedContainer}>
+  <View style={styles.form}>
+    <Text style={styles.formText}>INGRESAR</Text>
+    <View style={styles.formInput}>
+      <Image style={styles.formIcon} source={require('./assets/email.png')} />
+      <TextInput
+        style={styles.formTextInput}
+        placeholder='Correo electrónico'
+        keyboardType='email-address'
+      />
+    </View>
+    <View style={styles.formInput}>
+      <Image style={styles.formIcon} source={require('./assets/password.png')} />
+      <TextInput
+        style={styles.formTextInput}
+        placeholder='Contraseña'
+        keyboardType='default'
+        secureTextEntry={true}
+      />
+    </View>
+    <View style={{ marginTop: 30 }}>
+    <RoundedButton text='ENTRAR' onPress={ () => ToastAndroid.show('HOLA!', 
+    ToastAndroid.SHORT)}/>
+    </View>
+    <View style={styles.formRegister}>
+      <Text>¿No tienes cuenta?</Text>
+      <Text style={styles.formRegisterText}>Regístrate</Text>
+    </View>
+  </View>
+</View>
+
+ </ScrollView>
+);
 
 const styles = StyleSheet.create({
-  container: {
+ container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+ },
+ image: {
+    flex: 1,
     justifyContent: 'center',
-  },
-});
+ },
+ heading: {
+    color: 'white',
+    fontSize: 22,
+    marginBottom: 400,
+    lineHeight: 54,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
+ },
+ button: {
+    backgroundColor: 'black',
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+ },
+ buttonText: {
+    color: 'white',
+    fontSize: 18,
+    textAlign: 'center',
+ },
+ italicText: {
+    fontStyle: 'italic',
+ },
+ roundedContainer: {
+   borderRadius: 10, 
+   overflow: 'hidden',
+   margin: 10,
+ },
+ form: {
+    padding: 20,
+    backgroundColor: '#fff',
+ },
+ formText: {
+    fontSize: 24,
+    marginBottom: 20,
+ },
+ formInput: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+ },
+ formIcon: {
+    width: 20,
+    height: 20,
+    marginRight: 10,
+ },
+ formTextInput: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 5,
+    padding: 10,
+ },
+ formRegister: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 20,
+ },
+ formRegisterText: {
+    color: 'blue',
+    marginLeft: 5,
+ },
+ headingContainer: {
+   alignItems: 'center',
+   marginBottom: 2,
+   padding: 25,
+ },
+ logoContainer: {
+   flexDirection: 'row',
+   marginLeft: 10, 
+ },
+ logoImage: {
+   width: 30,
+   height: 30,
+   marginLeft:10,
+   marginRight: 10,  
+ },
+ logoText: {
+   color: 'white',
+   fontSize: 20,
+   fontWeight: 'bold',
+ },
+ 
+ });
+
+export default App;
+
